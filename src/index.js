dc3609154a50c3c5adf9f2b174165630f1ad5dfc
João Pedro De Sousa Soares <90647844+JoaoPedroDSS@users.noreply.github.com>
2024-08-30 09:46:15 -0300

    uso jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
 
-const elemento = document.getElementById('root')
-ReactDOM.render('Olá React!!!', elemento)
+
+const tag = 'olá mundo'
+ReactDOM.render(
+    <div>
+        {tag}
+        Olá React!
+    </div>,
+    document.getElementById('root')
+)
+
+//ReactDOM.render('Olá React!!!', elemento)
 //render está obsoleto
+//duas formas de usar o elemento
