7b832ec40dfff66f98df917982bab3edd0925590
João Pedro De Sousa Soares <90647844+JoaoPedroDSS@users.noreply.github.com>
2024-09-05 09:51:24 -0300

    props

---
+++ src/components/basicos/Comparametro.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+export default function ComParametro(props){
+    const sub = props.subtitulo
+    const status = props.nota1>=7 ? ' Aprovado ' : ' Recuperação '
+    return <div>
+        <h2>{props.titulo}</h2>
+        <h3>{sub}</h3>
+        <p>
+            { props.nota1 }
+             { status } 
+        </p>
+    {/*quando vc colocar o valor de texto como os parametros/propriedades, vc pode passar valores pelo jsx do index*/}
+    </div>
+   
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/Comparametro'
 
 const tag = 'olá mundo'
 ReactDOM.render(
@@ -9,6 +10,7 @@ ReactDOM.render(
        <Primeiro>
        </Primeiro>
        {/*como pode ver o componente é chamado como uma tag*/}
+       <ComParametro titulo="bacana" subtitulo="interessante" nota1={ 8 }/>
     </div>,
     document.getElementById('root')
 )
